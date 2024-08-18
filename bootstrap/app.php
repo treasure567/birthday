f55b7c3e388e7881b339f345faae6a6183b11596
Treasure Uvietobore <uvietoboretreasure@gmail.com>
2024-08-18 19:28:16 +0100

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Console\Scheduling\Schedule;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,4 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule->command('send:birthday')->name('send_birthday')->daily();
+    })
+    ->create();
