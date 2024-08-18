ed3b425c93e918ee7e40a0ed59d09b02f4f3864b
Treasure Uvietobore <uvietoboretreasure@gmail.com>
2024-08-18 19:28:15 +0100

    Create WhatsAppOtp.php

---
+++ app/Models/WhatsAppOtp.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
+
+class WhatsAppOtp extends Model
+{
+    use HasFactory;
+
+    protected $table = 'whatsapp_otps';
+
+    protected $fillable = [
+        'otp',
+        'whatsapp'
+    ];
+
+    protected $casts = [
+        'otp' => 'hashed'
+    ];
+
+    public function verifyOtp(int $otp) : bool {
+        if (!Hash::check($otp, $this->otp)) {
+            return false;
+        }
+        $this->resetOtp();
+        return true;
+    }
+
+    public function resetOtp(int $otp = null) : bool {
+        $this->otp = ($otp == null) ? generate_string(6, 'numeric') : $otp;
+        return $this->save();
+    }
+
+    public function sendWhatsAppOtp($msg, $whatsapp = null) : bool {
+        $whatsapp = $whatsapp ?? $this->whatsapp;
+        $otp = generate_string(6, 'numeric');
+        info($otp);
+        $msg = str_replace('{{otp}}', $otp, $msg);
+        if (trenalyze($whatsapp, $msg)) {
+            $this->resetOtp($otp);
+            return true;
+        }
+        return true;
+    }
+}
